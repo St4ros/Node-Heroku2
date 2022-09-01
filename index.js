71859ebe5dea8ea4ec069a55ff4decb996beccf4
St4ros <dstevenark@gmail.com>
2022-09-01 05:56:49 -0500

    master

--- index.js
+++ index.js
@@ -19,3 +19,46 @@ app.get("/", function (req, res) {
 });
 
 app.listen(process.env.PORT || 5000);
+
+
+app.get("/login",(req,res)=>{    
+  const getRegistro = async () =>{
+      try {
+          await pool.connect();
+          const reg = await pool.query('select * from registro');//(await) es para decir que es asincrona y que se ejecute mientras algo mas se ejecute
+                                                               //(query) es para traer o llamar las cosas de la base de datos
+          res.send(reg.rows);
+          //await pool.end();//sierra la base de datos
+          return reg;
+      } catch (e) {
+          console.log(e);
+      }
+  };
+  getRegistro();
+});
+
+app.post("/registro", (req,res)=>{  
+  let datos = req.body;
+  let nombre = ""+req.body.nombre;
+  //
+  console.log('me llego algoooo!!!');
+  console.log(req.body);
+  console.log(nombre);
+  //
+  const setRegistro = async()=>{
+      try {
+          await pool.connect();
+          const Registro_bd ='insert into registro(nombre,apellido,correo,fecha,contraseña,formulario,formulario2) VALUES($1,$2,$3,$4,$5,$6,$7)';
+          const balores = [req.body.nombre1,req.body.apellido1,req.body.gmail,req.body.fechanaci1,req.body.password1,'a','c'];
+          const reg = await pool.query(Registro_bd,balores);
+          console.log('si inserto la tabla');
+          res.send("usuario registrado con exito");
+          await pool.end();
+      } catch (e) {
+          res.send("El correo ya existe");
+          console.log('El correo ya existe');
+          await pool.end();
+      }
+  };
+  setRegistro();
+});
